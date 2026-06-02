39589f2670b47dda5482efc166bb4283fa83b2ac
karlahuarcayacar-cell <karlahuarcayacar@gmail.com>
2026-06-02 18:33:28 -0500

    primera actualizacion

---
+++ src/main/java/com/ensupunto/EnsupuntoApplication.java
@@ -0,0 +1,13 @@
+package com.ensupunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsupuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsupuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensupunto/EnsupuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensupunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsupuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
